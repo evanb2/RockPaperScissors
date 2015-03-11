b97230f5001f7a8974c86e415d72b6a86deca41a
Evan Butler & chitra atmakuru <student@epicodus.com>
2015-03-11 15:06:30 -0700

    add 'name=player1' inside form, as well as SESSION var in app.php

--- app/app.php
+++ app/app.php
@@ -2,6 +2,8 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RockPaperScissor.php";
 
+    session_start();
+
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -12,9 +14,18 @@
         return $app['twig']->render('form.twig');
     });
 
+    $app->get("/player1", function() use ($app) {
+        return $app['twig']->render('player1.twig');
+    });
+
+    $app->get("/player2", function() use ($app) {
+        $_SESSION['player1'] = $_GET['player1'];
+        return $app['twig']->render('player2.twig');
+    });
+
     $app->get("/results", function() use ($app) {
         $rockpaperscissors = new RockPaperScissor;
-        $player_inputs = $rockpaperscissors->gameRockPaperScissor($_GET['player1'], $_GET['player2']);
+        $player_inputs = $rockpaperscissors->gameRockPaperScissor($_SESSION['player1'], $_GET['player2']);
         return $app['twig']->render('results.twig', array('result' => $player_inputs));
     });
 
--- src/RockPaperScissor.php
+++ src/RockPaperScissor.php
@@ -6,11 +6,11 @@
             if ($player1 == null || $player2 == null) {
                 return "Ooops you forgot to enter a value";
             } elseif (($player1 == "rock" && $player2 == "paper") || ($player1 == "paper" && $player2 == "scissors") || ($player1 == "scissors" && $player2 == "rock")) {
-                return "Player 2";
+                return "Player 2 Wins!";
             } elseif ($player1 == $player2) {
                 return "Draw";
             } else {
-                return "Player 1";
+                return "Player 1 Wins!";
             }
 
         }
