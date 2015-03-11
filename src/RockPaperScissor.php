e3e750f45accfdfaf57b56f80952bf8655b9571f
Evan Butler & chitra atmakuru <student@epicodus.com>
2015-03-11 12:13:12 -0700

    fix if statement

--- src/RockPaperScissor.php
+++ src/RockPaperScissor.php
@@ -3,10 +3,12 @@
     {
         function gameRockPaperScissor($player1, $player2)
         {
-            if ($player1 == $player2) {
-                return "Draw";
+            if ($player1 == null || $player2 == null) {
+                return "Ooops you forgot to enter a value";
             } elseif (($player1 == "rock" && $player2 == "paper") || ($player1 == "paper" && $player2 == "scissors") || ($player1 == "scissors" && $player2 == "rock")) {
                 return "Player 2";
+            } elseif ($player1 == $player2) {
+                return "Draw";
             } else {
                 return "Player 1";
             }
